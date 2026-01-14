daf60c6766dc82eafba7184ae5f9913778e83dc9
KirillDomitin <domitin@gmail.com>
2026-01-14 23:56:02 +0300

    feat: рефакторинг get_participants

--- func/participants.py
+++ func/participants.py
@@ -9,14 +9,7 @@ def get_participants(data: dict) -> list:
     participants_common = data["СвУчредит"]
     participants = participants_common.get("УчрФЛ", [])
     if not isinstance(participants, list):
-        name = participants.get("СвФЛ").get("@attributes", {}).get("Имя", "")
-        surname = participants.get("СвФЛ").get("@attributes", {}).get("Фамилия", "")
-        patronymic = participants.get("СвФЛ").get("@attributes", {}).get("Отчество", "")
-        stock_share = participants.get("ДоляУстКап", {}).get("РазмерДоли", {}).get("Процент", "")
-        full_name = " ".join([surname, name, patronymic]).title()
-        participants_list.append("{} с долей {} процентов;".format(full_name, stock_share))
-
-        return participants_list
+        participants = [participants]
 
     for participant in participants:
         name = participant.get("СвФЛ").get("@attributes", {}).get("Имя", "")
