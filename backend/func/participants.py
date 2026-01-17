08a7874cd60a0d82ee3b3e360fc0e54fb8ae44c5
KirillDomitin <domitin@gmail.com>
2026-01-17 19:22:07 +0300

    fix: Если учредитель юл

--- backend/func/participants.py
+++ backend/func/participants.py
@@ -7,11 +7,15 @@ def get_participants(data: dict) -> list:
 
     participants_list = []
     participants_common = data["СвУчредит"]
-    participants = participants_common.get("УчрФЛ", [])
-    if not isinstance(participants, list):
-        participants = [participants]
+    participants_fl = participants_common.get("УчрФЛ", [])
+    participants_ul = participants_common.get("УчрЮЛРос", [])
+    if not isinstance(participants_fl, list):
+        participants_fl = [participants_fl]
 
-    for participant in participants:
+    if not isinstance(participants_ul, list):
+        participants_ul = [participants_ul]
+
+    for participant in participants_fl:
         name = participant.get("СвФЛ").get("@attributes", {}).get("Имя", "")
         surname = participant.get("СвФЛ").get("@attributes", {}).get("Фамилия", "")
         patronymic = participant.get("СвФЛ").get("@attributes", {}).get("Отчество", "")
@@ -19,4 +23,8 @@ def get_participants(data: dict) -> list:
         full_name = " ".join([surname, name, patronymic]).title()
         participants_list.append("{} с долей {} процентов;".format(full_name, stock_share))
 
+    for participant in participants_ul:
+        name = participant.get("НаимИННЮЛ", {}).get("@attributes", {}).get("НаимЮЛПолн")
+        stock_share = participant.get("ДоляУстКап", {}).get("РазмерДоли", {}).get("Процент", "")
+        participants_list.append("{} с долей {} процентов;".format(name, stock_share))
     return participants_list
