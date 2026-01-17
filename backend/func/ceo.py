60e74b2ea2f0d1f72a591c8bdeecfb16958f8a01
KirillDomitin <domitin@gmail.com>
2026-01-17 19:36:22 +0300

    fix: сео

--- backend/func/ceo.py
+++ backend/func/ceo.py
@@ -2,16 +2,17 @@
 # -*- coding: utf-8 -*-
 
 ceo_dict = {
-    "ЛИКВИДАТОР": "Ликвидатор общества",
-    "ГЕНЕРАЛЬНЫЙ": "Генеральный директор общества"
+    "ликвидатор": "Ликвидатор общества",
+    "генеральный": "Генеральный директор общества",
+    "генеральный директор": "Генеральный директор общества",
 }
 
 def get_ceo(data: dict)-> str:
 
     """ Получает генерального директора """
 
-    ceo_name_common = data.get("СведДолжнФЛ", {}) #.get("СвФЛ", {}).get("@attributes")
-    ceo_name = ceo_name_common.get("СвДолжн", {}).get("@attributes", {}).get("НаимДолжн", "").strip()
+    ceo_name_common = data.get("СведДолжнФЛ", {})
+    ceo_name = ceo_name_common.get("СвДолжн", {}).get("@attributes", {}).get("НаимДолжн", "").strip().lower()
     ceo = ceo_dict[ceo_name]
     name = ceo_name_common.get("СвФЛ").get("@attributes").get("Имя", "")
     surname = ceo_name_common.get("СвФЛ").get("@attributes").get("Фамилия", "")
