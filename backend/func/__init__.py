b482e0442d82f78622e888cef6525996b20dd2eb
KirillDomitin <domitin@gmail.com>
2026-01-17 16:59:46 +0300

    feat: асинхронность httpx

--- backend/func/__init__.py
+++ backend/func/__init__.py
@@ -7,3 +7,4 @@ from .charter_capital import get_charter_capital
 from .okved import get_okved
 from .participants import get_participants
 from .registration_date import get_registration_date
+from .request import get_response
