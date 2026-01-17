9545737138b1cf4de4397ff8604b3428f88b41e4
KirillDomitin <domitin@gmail.com>
2026-01-17 19:20:57 +0300

    fix: Добавил вариант если есть ликвидатор

--- backend/func/ceo.py
+++ backend/func/ceo.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+ceo_dict = {
+    "ЛИКВИДАТОР": "Ликвидатор общества",
+    "ГЕНЕРАЛЬНЫЙ": "Генеральный директор общества"
+}
+
 def get_ceo(data: dict)-> str:
 
     """ Получает генерального директора """
 
-    ceo_name_common = data.get("СведДолжнФЛ", {}).get("СвФЛ", {}).get("@attributes")
-    name = ceo_name_common.get("Имя")
-    surname = ceo_name_common.get("Фамилия")
-    patronymic = ceo_name_common.get("Отчество", "")
-    return f"{surname} {name} {patronymic}".title()
+    ceo_name_common = data.get("СведДолжнФЛ", {}) #.get("СвФЛ", {}).get("@attributes")
+    ceo_name = ceo_name_common.get("СвДолжн", {}).get("@attributes", {}).get("НаимДолжн", "").strip()
+    ceo = ceo_dict[ceo_name]
+    name = ceo_name_common.get("СвФЛ").get("@attributes").get("Имя", "")
+    surname = ceo_name_common.get("СвФЛ").get("@attributes").get("Фамилия", "")
+    patronymic = ceo_name_common.get("СвФЛ").get("@attributes").get("Отчество", "")
+    return f"{ceo}: {surname} {name} {patronymic}".title()
